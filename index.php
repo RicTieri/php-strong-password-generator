456045677dee89e599929595ca2cb0a6f18016a5
RicTieri <tieri.riccardo@gmail.com>
2024-01-18 17:30:32 +0100

    updated ui

--- index.php
+++ index.php
@@ -33,23 +33,44 @@
         </div>
         <div class="col-12">
           <div class="card col-12 px-3 py-4">
-            <div class="row">
-              <div class="col-7">
-                <label for="length" class="form-label mb-3 d-block">Lunghezza password:</label>
-                <label class="form-check-label" for="repeat_char">Consenti ripetizioni di uno o più caratteri:</label>
-              </div>
-              <div class="col-5">
-                <form>
-                  <div class="mb-3">
+              <form action="./index.php" method="GET" class="row">
+                <div class="col-7">
+                  <div class="mb-5 pb-5">
+                    <label for="length" class="form-label mb-3 d-block">Lunghezza password:</label>
+                    <label class="form-check-label" for="repeat_char">Consenti ripetizioni di uno o più
+                      caratteri:</label>
+                  </div>
+                  <div class="mt-5 pt-5">
+                    <button type="submit" class="btn btn-primary">Invia</button>
+                    <button type="reset" class="btn btn-secondary">Annulla</button>
+                  </div>
+                </div>
+                <div class="col-5 pe-5">
+                  <div class="mb-3 pe-5">
                     <input type="number" class="form-control" id="length" name="length">
                   </div>
-                  <div class="mb-3 form-check">
-                    <input type="checkbox" class="form-check-input" id="repeat_char" name="repeat_char">
+                  <div class="form-check">
+                    <input class="form-check-input" type="radio" name="repeat_char" id="repeat_char">
+                    <label class="form-check-label" for="repeat_char">Si</label>
                   </div>
-                  <button type="submit" class="btn btn-primary">Submit</button>
-                </form>
-              </div>
-            </div>
+                  <div class="form-check mb-3">
+                    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked>
+                    <label class="form-check-label" for="flexRadioDefault2">No</label>
+                  </div>
+                  <div>
+                    <input type="checkbox" class="form-check-input me-2" id="letter" name="letter">
+                    <label class="form-check-label" for="letter">Lettere</label>
+                  </div>
+                  <div>
+                    <input type="checkbox" class="form-check-input me-2" id="number" name="number">
+                    <label class="form-check-label" for="number">Numeri</label>
+                  </div>
+                  <div>
+                    <input type="checkbox" class="form-check-input me-2" id="symbol" name="symbol">
+                    <label class="form-check-label" for="symbol">Simboli</label>
+                  </div>
+                </div>
+              </form>
           </div>
         </div>
       </div>
